e34d5b4a84037d3949a1f9e6ad50e19c0fa60e8c
Vannya <van.tabbert@gmail.com>
2017-12-26 21:33:05 -0600

    about page updated

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,42 +1,64 @@
 import React from "react";
 import PageHeader from "../common/PageHeader";
-import rocks from '../../stylesheets/assets/VanRocks-300x300.jpg';
-import shrine from '../../stylesheets/assets/VanShrine-300x300.jpg';
-import littlevan from '../../stylesheets/assets/Little-Van.jpg';
+import IconBlurb from "../common/IconBlurb";
+import ProgressBar from "../common/ProgressBar";
+import rocks from "../../stylesheets/assets/VanRocks-300x300.jpg";
 
 const About = props => {
   return (
     <div id="about" className="about">
       <PageHeader text="About Me" />
-      <p>
-        I am an aspiring Software Developer rounding the end of my AAS in
-        Software Development. After dabbling in code for years, I decided that a
-        career transition must happen to follow my passion. In the meantime, I’m
-        spending all my free time taking courses, learning everything I can and
-        expanding my professional portfolio. I live in Dallas, TX currently, but
-        have lived around the US and Japan. I get wanderlust and, at 5 years,
-        have definitely been in this city far too long. Hopefully, relocation
-        will be part of my next career move! Until then, I’ll spend as much time
-        traveling and hiking as I can. You can check out my resume for my
-        education and employment background. Unfortunately, that only tells the
-        story of “Professional Van”. If you really want to know me, you’ll learn
-        much more over a cup of coffee. My interests are definitely varied.
-        Sewing, cooking, reading, playing a smattering of video and board games
-        keep me entertained. However, if I really want to enjoy myself, I read
-        and listen to audiobooks on a variety of topics, such as philosophy,
-        eastern religions, preparedness, self reliance, personal discipline,
-        radical simplicity, minimalism…..and of course, too many sci-fi novels.
-      </p>
-      <div className="img-group">
-        <img className="canhide" src={shrine} alt="Van and daughter at Shrine" />
-        <img className="canhide" src={littlevan} alt="Little Van"  />
-        <img src={rocks} alt="Bouldering Van"  />
+      <div className="about-center">
+        <div className="img-group">
+          <img src={rocks} alt="" />
+          <h2>Meet Van</h2>
+          <p>
+            I'm a Front-End Developer interested in creating efficient,
+            mobile-ready web applications with engaging user interfaces.
+          </p>
+        </div>
+        <div className="bar-group">
+          <ProgressBar label="React" width="80%" />
+          <ProgressBar label="Javascript" width="80%" />
+          <ProgressBar label="CSS/Sass" width="60%" />
+          <ProgressBar label="HTML" width="90%" />
+          <ProgressBar label="MongoDB" width="60%" />
+          <ProgressBar label="SQL" width="50%" />
+          <ProgressBar label="Express and Node.js" width="70%" />
+          <ProgressBar label="Java" width="60%" />
+          <ProgressBar label="Photoshop" width="80%" />
+          <ProgressBar label="Git/Version Control" width="80%" />
+        </div>
       </div>
-      <p>
-      When my hands are not attached to my keyboard, you’ll more than likely
-      find me at the gym, yoga studio or traveling around the US.
-    </p>
-    { window.location.pathname === "/" ? <a href="#cta"><i className="fas fa-angle-down"></i></a> : ""}
+      <div className="about-lower">
+        <IconBlurb
+          iconName="fas fa-user fa-2x"
+          text="UI/UX Focused"
+          blurb="Words Words"
+        />
+        <IconBlurb
+          iconName="fas fa-user fa-2x"
+          text="UI/UX Focused"
+          blurb="Words Words"
+        />
+        <IconBlurb
+          iconName="fas fa-user fa-2x"
+          text="UI/UX Focused"
+          blurb="Words Words"
+        />
+        <IconBlurb
+          iconName="fas fa-user fa-2x"
+          text="UI/UX Focused"
+          blurb="Words Words"
+        />
+      </div>
+      {window.location.pathname === "/" ? (
+        <a href="#cta">
+          <i className="fas fa-angle-down" />
+        </a>
+      ) : (
+        ""
+      )}
     </div>
   );
 };
