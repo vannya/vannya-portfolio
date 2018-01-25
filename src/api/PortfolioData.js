c63802bfb7d220ee2955ba590794070d73ca3e43
Vannya <van.tabbert@gmail.com>
2018-01-24 20:59:01 -0600

    added junipermail and style updates

--- src/api/PortfolioData.js
+++ src/api/PortfolioData.js
@@ -1,13 +1,28 @@
 module.exports = {
   portfolio: [
     {
+      pagename: "junipermail",
+      name: "JuniperMail",
+      description: "Survey creation application with a MERN framework.  Features include Stripe integration, Sendgrid integration (for webhooks), survey metrics and email templates for quick setup.",
+      livelink: "https://junipermail.herokuapp.com",
+      github: "https://github.com/vannya/junipermail",
+      img: "junipermail.jpg"
+    },
+    {
       pagename: "momentum",
       name: "Momentum Clone",
-      description:
-        "Clone of a popular productivity Chrome Extension using React, Firebase and external APIs.  Features include a Pomodoro timer, daily quotes, local weather, bookmarks and agenda/checklist.  The live demo is a stand alone build of the extension for demonstration purposes.",
+      description: "Clone of a popular productivity Chrome Extension using React, Firebase and external APIs.  Features include a Pomodoro timer, daily quotes, local weather, bookmarks and agenda/checklist.  The live demo is a stand alone build of the extension for demonstration purposes.",
       livelink: "https://vannya.github.io/momentum-clone/",
       github: "https://github.com/vannya/momentum-clone",
       img: "momentumclone.JPG"
+    },
+    {
+      pagename: "bearbnb",
+      name: "BearBnb",
+      description: "Currently, developing a clone of the popular site Airbnb using MERN stack as a 4 person remote team.  Responsible for entire backend, database structure, application architecture, OAuth implementation and user admin pages.  Additionally, I created the navigation and main overview sections of the home and host pages.  Acting Project Manager/Lead Developer on the site.",
+      livelink: "https://bearbnb-06.herokuapp.com",
+      github: "https://github.com/chingu-voyage3/bears-06",
+      img: "bearbnb.JPG"
     },
     {
       pagename: "corporatesite",
@@ -32,14 +47,6 @@ module.exports = {
       livelink: "https://ckingbailey.github.io/",
       github: "https://github.com/chingu-coders/Voyage2-Turtles-01",
       img: "turtrello.JPG"
-    },
-    {
-      pagename: "bearbnb",
-      name: "BearBnb",
-      description: "Currently, developing a clone of the popular site Airbnb using MERN stack as a 4 person remote team.  Responsible for entire backend, database structure, application architecture, OAuth implementation and user admin pages.  Additionally, I created the navigation and main overview sections of the home and host pages.  Acting Project Manager/Lead Developer on the site.",
-      livelink: "https://bearbnb-06.herokuapp.com",
-      github: "https://github.com/chingu-voyage3/bears-06",
-      img: "bearbnb.JPG"
     }
   ]
 };
