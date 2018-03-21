6ef07dcd76f208951386b64b56e717585c9da6b2
Vannya <van.tabbert@gmail.com>
2018-03-21 05:17:51 -0500

    daily log page and admin panel

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -1,8 +1,16 @@
 import React from 'react';
 
-const Button = (props) => {
+const Button = ({coloration, text, onClick, type}) => {
+  let buttonColorType;
+  if(coloration === "white") {
+    buttonColorType = 'btn';
+  } else if (coloration === "submit-btn") {
+    buttonColorType = 'submit-btn';
+  }else {
+    buttonColorType = 'btn-color';
+  }
   return (
-    <button className={props.coloration === 'white' ? "btn" : "btn-color"}>{props.text}</button>
+    <button type={type} className={buttonColorType} onClick={onClick}>{text}</button>
   );
 }
 
